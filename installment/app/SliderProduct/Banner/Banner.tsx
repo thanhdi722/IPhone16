bdaf4982fec84b8ec6bca3aaa8b8008fa8aa1626
thanhdi721 <thanhdi721@gmail.com>
2024-09-13 16:13:24 +0700

    update

--- installment/app/SliderProduct/Banner/Banner.tsx
+++ installment/app/SliderProduct/Banner/Banner.tsx
@@ -6,6 +6,7 @@ import banner from "@/app/Images/HeaderLP-Desktop-ROI-1920x600.png";
 import bannerMB from "@/app/Images/Banner-MB.png";
 import Image from "next/image";
 import { message } from "antd";
+
 const Banner: React.FC = () => {
   const handleClick = () => {
     message.info("Hi");
@@ -13,6 +14,7 @@ const Banner: React.FC = () => {
   const handleClick1 = () => {
     message.info("Ha");
   };
+
   return (
     <div>
       <Image
@@ -36,18 +38,7 @@ const Banner: React.FC = () => {
           id="navigationBar"
           className="bannerTopHead"
           style={{ height: "50px", backgroundColor: "#313131" }}
-        >
-          {/* className="cps-container is-flex is-justify-content-space-between is-align-items-center" */}
-          <div className={style.cps_container}>
-            <div className="navigation-brand"></div>
-            <div className="navigation-wrapper">
-              <a className="navigation-item active">ƯU ĐÃI</a>
-              <a className="navigation-item">THU CŨ</a>
-              <a className="navigation-item">TÍNH NĂNG</a>
-              <a className="navigation-item">ĐĂNG KÝ NGAY</a>
-            </div>
-          </div>
-        </div>
+        ></div>
       </div>
     </div>
   );
--- installment/app/SliderProduct/page.tsx
+++ installment/app/SliderProduct/page.tsx
@@ -3,11 +3,13 @@ import Section1 from "./Section1/Section1";
 import Section2 from "./Section2/Section2";
 import Section3 from "./Section3/Section3";
 import Banner from "./Banner/Banner";
+import Navbar from "./NavBar/Navbar";
 
 function page() {
   return (
     <div>
       <Banner />
+      <Navbar />
       <Section1 />
       <Section2 />
       <Section3 />
